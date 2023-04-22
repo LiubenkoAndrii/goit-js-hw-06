e75c4db75f648fab0a3337ef773f8efd14ff8576
Andrii Liubenko <lubenkoandrew@gmail.com>
2023-04-23 01:34:57 +0300

    add-task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("All fields must be filled!");
+  } else {
+    const formData = {
+      email,
+      password,
+    };
+    console.log(formData);
+    event.currentTarget.reset();
+  }
+}
